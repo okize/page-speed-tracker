fa7b350afe3240c15fe199a859559ff43fbc46ae
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-01-21 12:43:04 -0500

    added saveResults module

---
+++ saveResults.js
@@ -0,0 +1,23 @@
+// modules
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var moment = require('moment');
+
+// save score results to disk
+var saveResults = function (results) {
+
+  var allScores = {
+    timestamp: moment().format(),
+    results: results
+  };
+
+  var filename = 'data/' + 'pageSpeedScores_' + moment().format('YYYYMMDD') + '.json';
+
+  fs.writeFile(filename, JSON.stringify(allScores, null, 2), function (err) {
+    if (err) throw err;
+    console.log(filename + ' saved!');
+  });
+
+};
+
+module.exports = saveResults;

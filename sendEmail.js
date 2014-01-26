a1adbd101afbad2e5dc76af1dba0d91649457eed
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-01-26 09:19:08 -0500

    rewrote sendEmail so arguments can be passed though command line or stored in env vars

--- sendEmail.js
+++ sendEmail.js
@@ -1,25 +1,37 @@
-var nodemailer = require('nodemailer');
+#!/usr/bin/env node
 
-var smtpTransport = nodemailer.createTransport('SMTP', {
+// pass email subject as first argument and email body as second argument
+
+var email = {
   service: 'Gmail',
+  user: process.argv[4] || process.env['GMAIL_USER'],
+  password: process.argv[5] || process.env['GMAIL_PASSWORD'],
+  from: process.argv[4] || process.env['GMAIL_FROM'],
+  to: process.argv[6] || process.env['GMAIL_TO'],
+  subject: process.argv[2] || '',
+  text: process.argv[3] || ''
+};
+
+var smtpTransport = require('nodemailer').createTransport('SMTP', {
+  service: email.service,
   auth: {
-    user: process.env['GMAIL_USER'],
-    pass: process.env['GMAIL_PASSWORD']
+    user: email.user,
+    pass: email.password
   }
 });
 
 smtpTransport.sendMail({
-  from: process.env['GMAIL_FROM'],
-  to: process.env['GMAIL_TO'],
-  subject: 'Ping',
-  text: 'Pong'
+  from: email.from,
+  to: email.to,
+  subject: email.subject,
+  text: email.text
 }, function (err, res){
 
   if (err) {
     return console.error(err);
   }
 
-  console.log('Message sent: ' + res.message);
+  console.log(res.message);
 
   smtpTransport.close();
 

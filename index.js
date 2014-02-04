83703ad904189250ce3127f425745b23cdac2e0a
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-02-04 14:42:13 -0500

    new index

---
+++ index.js
@@ -0,0 +1,16 @@
+var path = require('path'),
+    cronJob = require('cron').CronJob,
+    time = require('time');
+
+var email = require('./sendEmail.js');
+
+var job = new cronJob({
+  cronTime: '*/1 * * * *',
+  onTick: function() {
+    email('email subject', 'email body');
+  },
+  start: false,
+  timeZone: "America/New_York"
+});
+
+job.start();

4e20f01cf817f693437790edecf104dc21be3ba3
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-02-04 20:40:20 -0500

    updates app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var urls = require(path.join(__dirname, 'lib', 'urls.json'));
 
 // sets up cron job for getting & saving page speed scores
 var job = new cronJob({
-  cronTime: '38 20 * * *',
+  cronTime: '45 20 * * *',
   // cronTime: '0 8 * * *',
   // cronTime: process.env['CRON_TIME'],
   onTick: function () {
@@ -23,5 +23,7 @@ var job = new cronJob({
 
 function getScores () {
   var data = require(path.join(__dirname, 'lib', 'temp_example.json'));
-  email('Page speed scores saved', data);
+  email('Page speed scores saved [form: ' + process.env['USER'] + ']', data);
 }
+
+console.log(process.env);

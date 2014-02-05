74562f70c67c82cbd62fadfafebb58d917cd555b
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-02-04 19:45:39 -0500

    updates app with html email from file

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-var path = require('path'),
-    cronJob = require('cron').CronJob,
-    time = require('time');
+var path = require('path');
+var fs = require('fs');
+var cronJob = require('cron').CronJob;
+var time = require('time');
 
-var email = require('./sendEmail.js');
+//
+var email = require(path.join(__dirname, 'lib', 'sendEmail.js'));
 
+// urls to retreive page speed scores for
+// var urls = require(path.join(__dirname, 'lib', 'urls.json'));
+
+//
 var job = new cronJob({
-  cronTime: '0 8 * * *',
+  cronTime: '49 18 * * *',
+  // cronTime: process.env['CRON_TIME'],
   onTick: function() {
-    email('email sent', 'email body');
+
+    fs.readFile('./test.html', function (err, data) {
+      if (err) throw err;
+      email('email sent', data);
+    });
+
   },
   start: false,
-  timeZone: "America/New_York"
+  timeZone: 'America/New_York'
 });
 
 job.start();

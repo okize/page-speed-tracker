9f0647adccd00a0a2c8a0c435284500458c78e8d
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-02-10 19:27:04 -0500

    brings app.js back to previous state

--- app.js
+++ app.js
@@ -1,13 +1,85 @@
-var connectionString = process.env['DATABASE_URL'];
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize(connectionString, {
-  dialect: 'postgres'
-});
+// modules
+var Promise = require('bluebird');
+var path = require('path');
+var fs = require('fs');
+var moment = require('moment');
+var cronJob = require('cron').CronJob;
+var time = require('time');
 
-sequelize.query('SELECT * FROM "Urls"').success(function(rows) {
-  var urls = [];
-  for (var i = 0, len = rows.length; i < len; i++) {
-    urls.push(rows[i].url);
-  }
-  console.log(urls);
+// timer vars
+var timerStart = new Date().getTime();
+
+// for sending notification emails
+var email = require(path.join(__dirname, 'lib', 'sendEmail.js'));
+var save = require(path.join(__dirname, 'lib', 'saveResults.js'));
+
+// filename to save
+var filename = 'pageSpeedScores_' + moment().format('YYYYMMDD') + '.json';
+
+// for requesting page speed scores
+var getScore = require(path.join(__dirname, 'lib', 'getScore.js'));
+
+// query db for list of urls to get scores for
+var getUrls = require(path.join(__dirname, 'lib', 'getUrls.js'));
+
+// google page speed insights api key
+var apiKey = process.env['PAGESPEED_API_KEY'];
+
+// scoring strategies for page speed insights
+var strategies = ['mobile', 'desktop'];
+
+// get all the pagespeed scores, save them & email them
+var getScores = function () {
+
+  getUrls().then(function(rows) {
+
+    var urls = [];
+    for (var i = 0, len = rows.length; i < len; i++) {
+      urls.push(rows[i].url);
+    }
+    return urls;
+
+  }).map(function (url) {
+
+    return Promise.all(
+      strategies.map(function (strategy) {
+        return getScore(url, strategy, apiKey).spread(function (res, body) {
+          var score = {url: url};
+          score[strategy + 'Score'] = JSON.parse(body).score;
+          return score;
+        });
+      })
+    );
+
+  }).then(function (results) {
+
+    // temporary
+    save(results, filename);
+
+    var timeCount = (new Date().getTime() - timerStart)/1000;
+
+    var data = {
+      timestamp: moment().format(),
+      timer: timeCount,
+      results: results
+    };
+    return email('Page speed scores saved', data);
+
+  }).catch(function (err) {
+
+    console.error('Error thrown: ' + err);
+
+  });
+
+};
+
+// sets up cron job for getting & saving page speed scores
+var job = new cronJob({
+  cronTime: '0 8 * * *',
+  // cronTime: process.env['CRON_TIME'],
+  onTick: function () {
+    return getScores();
+  },
+  start: true,
+  timeZone: 'America/New_York'
 });

2a55585dd02f8681c33188a83d89922bf79adbaa
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-02-14 09:02:43 -0500

    update cron time again

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var time = require('time');
 var timerStart = new Date().getTime();
 
 //  time to run app in cron format
-var timeToRun = '0 9 * * *';
+var timeToRun = '10 9 * * *';
 
 // for sending notification emails
 var email = require(path.join(__dirname, 'lib', 'sendEmail.js'));

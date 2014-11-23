6cf0bfa679d52963ca5962a155e9f622f98fb08c
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-11-23 16:33:37 -0500

    order results and format date

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var _ = require('lodash');
 var models = require('../models');
 
 router.get('/', function(req, res) {
   models.Score
-    .findAll()
-    .then(function(response) {
+    .findAll({
+      where: {
+        url:'http://www.patientslikeme.com/'
+      },
+      order: [
+        ['id', 'DESC']
+      ]
+    })
+    .then(function(scores) {
       res.render('index', {
         title: 'Page Speed Tracker',
-        json: JSON.stringify(response, null, 2)
+        scores: scores
       });
     });
 });

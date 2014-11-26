14c1007a462d8d27b2c2330e0fdad1bc94122c34
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-11-25 19:54:22 -0500

    add a score api and load to client

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,1 +1,15 @@
-console.log('Page Speed Tracker');
+// get chart data
+$.ajax({
+  type: 'GET',
+  contentType: 'application/json',
+  url: '/api/score/1',
+  success: function(data) {
+    console.log(data);
+  },
+  error: function(error){
+    console.log(error.responseText);
+  },
+  complete: function() {
+    console.log('Scores loaded');
+  }
+});
--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,47 @@ var models = require('../models');
 router
   .route('/score')
   .get(function (req, res) {
-    res
-      .status(200)
-      .send('needs url id');
+    models.Score
+      .findAll()
+      .then(function(scores) {
+        res
+          .status(200)
+          .json({
+            scores: scores
+          });
+      });
+  });
+
+router
+  .route('/score/:urlId')
+  .get(function (req, res) {
+    models.Url
+      .find(req.params.urlId)
+      .then(function(urls) {
+        var url = urls.dataValues.url;
+        models.Score
+          .findAll({
+            where: {
+              url: url
+            },
+            order: [
+              ['id', 'DESC']
+            ]
+          })
+          .then(function(scores) {
+            res
+              .status(200)
+              .json({
+                urlId: req.params.urlId,
+                url: url,
+                scores: scores
+              });
+          });
+      })
+      .error(function(error) {
+        console.error(error);
+        res.status(404).send('No scores found!');
+      });
   });
 
 module.exports = router;

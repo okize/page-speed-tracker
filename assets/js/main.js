5de25bd3eb1be1dcf9cb41976591d28abb6a6839
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-11-28 18:16:37 -0500

    refactor charts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // creates a div target for a chart
 function insertChartTarget (id) {
-  $('body').append('<div id="chart_' + id + '" class="text-center extended-y-ticks"></div>');
+  $('body').append('<div id="chart_' + id + '" class="text-center extended-y-ticks extended-x-ticks"></div>');
 }
 
 // inserts a new chart based on a url id
@@ -16,21 +16,43 @@ function initCharts (urlIds) {
         scores[i] = convert_dates(scores[i], 'date');
       }
 
+      var release_markers = [
+        {'date': new Date('2014-04-08'), 'label': '???'},
+        {'date': new Date('2014-06-21'), 'label': '???'},
+        {'date': new Date('2014-07-01'), 'label': '???'},
+        {'date': new Date('2014-07-09'), 'label': '???'},
+        {'date': new Date('2014-10-01'), 'label': '???'},
+        {'date': new Date('2014-10-21'), 'label': '???'}
+      ];
+
       // add a wide multi-line chart
       data_graphic({
+        data: scores,
+        x_accessor: 'date',
+        y_accessor: 'score',
+        target: '#chart_' + obj.id,
         chart_type: 'line',
+        markers: release_markers,
+        animate_on_load: false,
+        area: false,
+        // custom_line_color_map: [1,4],
+        interpolate: 'step',
         title: data.url,
+        // description: "Chart description here.",
         legend: ['Mobile', 'Desktop'],
         legend_target: '#legend',
+        list: false,
         linked: true,
-        data: scores,
-        width: 1000,
-        height: 300,
         show_years: true,
+        xax_count: 12,
+        // xax_tick_length: 10,
+        show_years: true,
+        x_extended_ticks: true,
         y_extended_ticks: true,
-        target: '#chart_' + obj.id,
-        x_accessor: 'date',
-        y_accessor: 'score'
+        width: 1200,
+        height: 300,
+        right: 0,
+        left: 30
       });
 
     });
